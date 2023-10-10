cd441aeab164d3fab9ea575df98eae65321da1db
ThankyouTaro <54502909+ThankyouTaro@users.noreply.github.com>
2023-10-10 13:37:49 +0200

    Upload Inazuma Slime

---
+++ Assets/Scripts/Behaviour/RotateTrail.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTrail : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject trial1, trial2, trial3;
+    float radius = 2f, radspeed = 0.02f;
+    float radCounter = 0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        radCounter += radspeed;
+        trial1.transform.position = transform.position + new Vector3(radius * Mathf.Cos(radCounter), 0f, radius * Mathf.Sin(radCounter));
+        trial2.transform.position = transform.position + new Vector3(radius * Mathf.Cos(radCounter + Mathf.Deg2Rad * 120f), 0f, radius * Mathf.Sin(radCounter + Mathf.Deg2Rad * 120f));
+        trial3.transform.position = transform.position + new Vector3(radius * Mathf.Cos(radCounter + Mathf.Deg2Rad * 240f), 0f, radius * Mathf.Sin(radCounter + Mathf.Deg2Rad * 240f));
+
+    }
+}
